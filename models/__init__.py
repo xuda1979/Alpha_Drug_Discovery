490cf79a04f8ee54433e976e1a1b40e90319a3c7
David Hsu <exutechnology@gmail.com>
2025-05-31 05:13:26 -0400

    Add GNN model and dataset utilities

--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,6 @@
 from .gan_drug_design import train_gan
 from .rl_drug_design import train_policy_gradient
 from .deep_docking import train_docking_model
+from .gnn_property_prediction import train_gcn
 
-__all__ = ['train_gan', 'train_policy_gradient', 'train_docking_model']
+__all__ = ['train_gan', 'train_policy_gradient', 'train_docking_model', 'train_gcn']
